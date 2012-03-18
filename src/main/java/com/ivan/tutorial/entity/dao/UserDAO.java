0bfdfdbf799c323b4e19bd340cb5768a4eaed79c
Ivan <kareshi@gmail.com>
2012-03-18 22:51:37 +0800

    HibernateTemplate

--- src/main/java/com/ivan/tutorial/entity/dao/UserDAO.java
+++ src/main/java/com/ivan/tutorial/entity/dao/UserDAO.java
@@ -1,27 +1,14 @@
 package com.ivan.tutorial.entity.dao;
 
 import com.ivan.tutorial.entity.User;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 import java.util.List;
 
-public class UserDAO {
+public interface UserDAO {
 
-    private DataSource dataSource;
+    void create(User user);
 
-    public void create(User user) {
-        JdbcTemplate insert = new JdbcTemplate(dataSource);
-        insert.update("INSERT INTO USERS (ID, CREATEDBY, CREATEDON, NAME, PASSWORD) VALUES(?,?,?,?,?)",
-                new Object[] { user.getId(), user.getCreatedBy(), user.getCreatedOn(), user.getName(), user.getPassword() });
-    }
+    List<User> selectAll();
 
-    public List<User> selectAll() {
-        JdbcTemplate select = new JdbcTemplate(dataSource);
-        return select.query("SELECT * FROM USERS", new UserRowMapper());
-    }
 
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 }
